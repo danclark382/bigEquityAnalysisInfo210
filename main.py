0274d9e6a7530b1e6d809cf69ca7e6f041bdc876
Dan Clark <38151937+danclark382@users.noreply.github.com>
2019-10-23 02:52:05 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import csv
-from iexfinance.stocks import Stock
-
-
-def get_stock(ticker):
-    stock = Stock(ticker, output_format='pandas', token='sk_e055e4da353343a5ada52ed1f9ac42ca')
-    stock.to_csv(ticker + '.csv')
-    
-    
