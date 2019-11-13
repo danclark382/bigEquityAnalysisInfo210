d7a7da372d3017e9568ba8a98cc0c095e568f0c9
djc382 <djc382@drexel.edu>
2019-11-13 11:48:48 -0500

    Edited iexData.py to mkdir

--- iexData.py
+++ iexData.py
@@ -6,31 +6,49 @@ import plotly.graph_objects as go
 from datetime import datetime
 import os
 
-print(os.getcwd())
 
-with open("../myKeys.json") as jsonFile:
+with open("myKeys.json") as jsonFile:
     data = json.load(jsonFile)
     IEX_TOKEN = data['keys']['iex']
 
+DATADIR = os.path.exists('data')
+
+def generateStock(ticker):
+    """Generate stock object to return
+
+	 Create a stock object from ticker to call iex functions
+	 :param ticker: Stock ticker
+	 :type ticker: String
+	 :return: stock object
+	 :return type: iexfinance.stocks.Stock()
+	 """
+    return Stock(ticker, token=IEX_TOKEN)
 
 def getPriceHistory(ticker):
     """Get the price history of ticker
 
-    Get the historical prices of a ticker starting from startDate through endDate
+    Get the historical prices of a ticker starting from startDate through endDate.
+	 Conver the dataframe to a CSV and save the file to a directory data/{ticker}/{ticker}.csv.
+	 If the directory data or data/ticker does not exit, make the directory.
     :param ticker: Stock ticker
     :type ticker: String
     :return: df of price history
     :return type: Dataframe()
-"""
+    """
     startDate = datetime(2015, 1, 1)
     endDate = datetime(2019, 12, 25)
-    token = IEX_TOKEN
-    df = get_historical_data(ticker, start=startDate, end=endDate, output_format='pandas', token=token)
+    df = get_historical_data(ticker, start=startDate, end=endDate, output_format='pandas', token=IEX_TOKEN)
+    if not DATADIR:
+        os.makedirs('data')
+        os.makedirs('data/' + ticker)
+        DATADIR = True
+    elif not os.path.exists('data/' + ticker):
+        os.makedirs('data/' + ticker)
     filename = 'data/' + ticker + '/' + ticker + '.csv'
     df.to_csv(filename)
     return df
 
-def plotDf(df,ticker):
+def plotDf(df, ticker):
     """Create a plot of the df timeseries
     
     Generates a matplot chart of the stocks price history based on closing price
@@ -55,7 +73,7 @@ def plotDf(df,ticker):
     return True
 
 
-def getHeadlines(ticker):
+def getHeadlines(stock, ticker):
     """Call IEX get_news to gather recent headlines for ticker.
 
     This method gets the most recent headlines for a ticker
@@ -63,11 +81,9 @@ def getHeadlines(ticker):
     :type ticker: String
     :return: dictionary consisting of (date, headline) key, value pairs
     :return type: dictionary"""
-    headlines = Stock("AAPL", token=IEX_TOKEN)
-    head = headlines.get_news()
-    print(head)
-    print(type(head))
-
+    # get_news() returns a list of dictionaries
+    headlines = stock.get_news()
+    return headlines
 
 def dataImport():
     """Main entry point for iexData
@@ -75,10 +91,11 @@ def dataImport():
     This method calls all functions that generate data and pipeline it to the respective directory.
     """
     stockList = pd.read_csv('sec_scrape/tickers.csv')
-    for stock in stockList['Ticker']:
-        #priceDf = getPriceHistory(stock)
-        #plotDf(priceDf, stock)
-        getHeadlines(stock)
+    for stockTicker in stockList['Ticker']:
+        iexStock = generateStock(stockTicker)
+        priceDf = getPriceHistory(stockTicker)
+        #plotDf(priceDf, stockTicker)
+        #getHeadlines(iexStock, stockTicker)
         break
 
 dataImport()
