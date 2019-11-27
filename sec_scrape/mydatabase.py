228a6faf9af7abd2c33133d8bdfb1b9ca8175132
Dan Clark <dac193@pitt.edu>
2019-11-27 15:18:39 -0500

    Removed mydatabase.py

--- sec_scrape/mydatabase.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client.admin
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
