c315a24e50dea5413dd4887d777c85ea9c834e1a
djc382 <djc382@drexel.edu>
2019-12-02 18:03:23 -0500

    edited

--- db.py
+++ db.py
@@ -26,6 +26,23 @@ def getFileList(dir):
     return dir+NEWS_HISTORY, dir+PRICE_HISTORY
 
 
+def convertList(lst):
+    convertedL = []
+    for i in lst:
+        convertedL.append(i)
+    return convertedL
+
+
+def getdocuments(dir):
+    fileList = []
+    splitFiles = []
+    for r, d, f in os.walk(os.getcwd()):
+        for i in f:
+            if not ('.csv' in i or '.json' in i):
+                splitFile = i.split('__')
+                fileList.append(splitFile)
+    return splitFiles
+
 def checkOS(path):
     if os.path.isfile(os.path.abspath(path)):
         return True
@@ -73,22 +90,38 @@ def insertDocument(mydb, collection, doc):
     documentIn = fs.put(doc)
 
 
+def insertdocument(mydb, doc):
+    """Insert a document into the mydb.collection"""
+    if len(doc) != 3:
+        return False
+    fs = gridfs.GridFS(mydb)
+    try:
+        date = doc[2].split('_')[-1]
+        documentIn = fs.put(doc, filename=doc[2], date=date)
+    except Exception as e:
+        return e
+    return True
+
+
 myclient = pymongo.MongoClient(f"mongodb://{mongoUser}:{mongoPassword}@dsci210-shard-00-00-fknts.mongodb.net:27017,dsci210-shard-00-01-fknts.mongodb.net:27017,dsci210-shard-00-02-fknts.mongodb.net:27017/test?ssl=true&replicaSet=dsci210-shard-0&authSource=admin&retryWrites=true&w=majority")
 mydb = myclient['dbTest']
 dirList = getDirList()
 for dir in dirList:
+    print(dir)
     try:
         os.chdir(dir)
     except NotADirectoryError:
         LOGGER.info(f'ERROR for {dir}')
         continue
     news, csv = getFileList(dir)
-    if csv:
-        priceDict = getCSV(csv)
-        if priceDict:
-            insertPriceAction(mydb, dir, priceDict)
-    if news:
-        newsDict = getNews(news)
-        if newsDict:
-            insertNews(mydb, dir, newsDict)
+    #if csv:
+    #    priceDict = getCSV(csv)
+    #    if priceDict:
+    #        insertPriceAction(mydb, dir, priceDict)
+    #if news:
+    #    newsDict = getNews(news)
+    #    if newsDict:
+    #        insertNews(mydb, dir, newsDict)
+    docs = getdocuments(dir)
+    print(docs)
     os.chdir('..')
